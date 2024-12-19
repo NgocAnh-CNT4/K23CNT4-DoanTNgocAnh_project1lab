37974b11e9e81aced993da7591f2807b00160231
unknown <doan80040@gmail.com>
2024-12-19 18:58:13 +0700

    K23CNT4-DOANTNGOCANH-PROJECT1-LAB

---
+++ K23CNT4_DoanTNgocAnh_2310900008_Project1/database/migrations/2024_12_19_112214_create__d_t_n_a__q_u_a_n__t_r_i_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('_d_t_n_a__q_u_a_n__t_r_i', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_d_t_n_a__q_u_a_n__t_r_i');
+    }
+};
---
+++ K23CNT4_DoanTNgocAnh_2310900008_Project1/database/seeders/DTNA_QUAN_TRITableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DTNA_QUAN_TRITableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
